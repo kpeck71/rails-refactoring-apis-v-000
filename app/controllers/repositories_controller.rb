b6c91a70c0e56a64edaeebf2549cf5f71d1f0d1f
kpeck71 <kpeck71@gmail.com>
2018-06-15 17:50:47 -0400

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,11 +1,13 @@
 class RepositoriesController < ApplicationController
   def index
-    response = Faraday.get "https://api.github.com/user/repos", {}, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
-    @repos_array = JSON.parse(response.body)
+    user = GithubService.new({ "access_token" => session[:token]} )
+    # @all_repos = user.get_repos
+    @repos_array = user.get_repos
   end
 
   def create
-    response = Faraday.post "https://api.github.com/user/repos", {name: params[:name]}.to_json, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
+    user = GithubService.new({ "access_token" => session[:token]} )
+    user.create_repo(params[:name])
     redirect_to '/'
   end
 end
